ac228b0951f91ad3f89352a6abc18c71e8309371
Mariana Terintii <117921869+marianaterintii@users.noreply.github.com>
2023-03-21 18:50:01 +0200

    Update HW.dynamic.class.py

--- HW.dynamic.class.py
+++ HW.dynamic.class.py
@@ -19,7 +19,7 @@ class Bank:
         if type (balance) != int:
             raise TypeError ("Only integer values accepted")
         
-    # to string
+    # to string 
     def __str__(self):
         return f" BANK: {self.__balance}"
 
@@ -28,33 +28,35 @@ class LocalBank(Bank):
         super().__init__(balance)                # delegation (first operation to do, beacause is delegating to first class)
         self.locality = locality                 # ( second operation beause is local logic)
       
-
       def __str__(self):
         return f" LOCAL BANK:({self.locality}): {self.getBalance()}"
 
 # HW2: new class < Creditbank with properties - max_credit_amount + credit_percentage(interest)
 class CreditBank (Bank):
-    def __init__(self, max_credit_amount, balance = 0 ): 
+    def __init__(self, max_credit_amount, credit_percentage, balance = 0 ): 
         super().__init__(balance) 
-        self.max_credit_amount, = max_credit_amount,
-        if type (max_credit_amount,) != int:
-            raise TypeError ("Only integer values accepted") 
+        self.check_balance_type(max_credit_amount)
+        self.max_credit_amount = max_credit_amount
+        self.credit_percentage = credit_percentage
+    
+    def monthlyPayment(self):
+        return self.max_credit_amount * self.credit_percentage / 100 / 12
 
-    def __str__(self):
-        return f" Max credit amount: {self.max_credit_amount}, Interest:{self.getBalance()}"
+    def __str__(self): # override to print all properties in a formatted string
+        return f" Max credit amount: {self.max_credit_amount}, Interest:{self.credit_percentage}%, Monthly payment: {self.monthlyPayment(): .2f}"
 
     
 ##############################
 world_bank = Bank (1_000_000_000_000)
 agro = LocalBank ("Chisinau", 1_000_000)
-credit = CreditBank( 1_000_000, 5000)
+credit = CreditBank( 1_000_000, 5)
 
 
 print(world_bank)
 print(agro)
 print(credit)
 
-# HW3:  - in proces
+# HW3:  
 
 '''
 +-------- main program --------------------------------------------------------+
@@ -63,27 +65,29 @@ print(credit)
 |  Bank -------> +--- < class > ------------+                                  |
 | ^    ^         |                          |                                  |
 | |    |         | .__init__(self.balance=0)| <--- world_bank -- +-- obj ---+  |
-| |    |         | .__str__(self)     ^     |                    | __balance|  |
-| |    |         | .check_balance_type|     |                    |    ^     |  |
-| |    |         |                    |     |                    +----|-----+  |
-| |    |    <------.getBalance() -----|-------------------------------+        |
-| |    |    ------>.setBalance()------|-------------------------------+        |
-| |    |         |                    |     |                                  |  
-| |    |         +--------------------|-----+                                  |
-| |    |                              |                                        |
-| |    |                              |<-delegation                            |
-| |    |                              |                                        |
-| | LocalBank -> +--- < class > ------|-----+                                  |
-| |              |                    |     |                                  |
-| |              | .__init__(self.    |     | <--- world_bank -- +-- obj ---+  |
-| |              |      locality, balance=0)|                    |__balance |  |
-| |              | .__str__(self)           |                    | .locality|  |
-| |              +--------------------------+                    +----------+  |
-| CreditBank ---> +--- < class > -----------+                                  |
-|                |                          |                                  |
-|                | .__init__(self.          | <--- world_bank -- +-- obj ---+  |
-|                |  .credit, balance=0)     |                    | .balance |  |
-|                | .__str__(self)           |                    | .credit  |  |
+| |    |         | .__str__(self)     ^ ^   |                    | __balance|  |
+| |    |         | .check_balance_type| |   |                    |    ^     |  |
+| |    |         |                    | |   |                    +----|-----+  |
+| |    |    <------.getBalance() -----|-|-----------------------------+        |
+| |    |    ------>.setBalance()------|-|-----------------------------+        |
+| |    |         |                    | |   |                                  |  
+| |    |         +--------------------|-|---+                                  |
+| |    |                              | |                                      |
+| |    |                              | | <- delegation                        |
+| |    |                              | -------                                |
+| | LocalBank -> +--- < class > ------|-----+ |                                |
+| |              |                    |     | |                                |
+| |              | .__init__(self.    |     | |<-- world_bank -- +-- obj ---+  |
+| |              |      locality, balance=0)| |                  |__balance |  |
+| |              | .__str__(self)           | |                  | .locality|  |
+| |              +--------------------------+ |                  +----------+  |
+| CreditBank ---> +--- < class > -----------+ |                                |
+|                |                          | |                                |
+|                | .__init__(self.  -----------                                |
+|                |  .credit,                | <--- world_bank -- +-- obj ---+  |
+|                |  .percentage, balance=0) |                    | .credit     |
+|                | .monthly_payment         |                    | .percentage |  
+|                | .__str__(self)           |                    | .monthly    |  
 |                +--------------------------+                    +----------+  |
 +------------------------------------------------------------------------------+
 '''
